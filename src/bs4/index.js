86e47c2922f35529411beda5e8f56d4eee68dd4b
Lee Nguyen <lee.nguyen@innovatube.com>
2017-11-27 22:50:17 +0700

    refactor

--- src/bs4/index.js
+++
@@ -1,8 +0,0 @@
-import XField from './Field'
-import XTextarea from './TextArea'
-import XSelect from './Select'
-import XCheckBoxes from './CheckBoxes'
-import XRadioButtons from './RadioButtons'
-import SaveButton from './SaveButton'
-
-export { XField, XTextarea, XSelect, XCheckBoxes, XRadioButtons, SaveButton }
